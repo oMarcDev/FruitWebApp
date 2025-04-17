54b062e4ca99136b8d65bfc23fc00b90e7036600
Marcelo <marcmiranda566@gmail.com>
2025-04-17 08:36:14 -0300

    First

---
+++ Models/FruitModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace FruitWebApp.Models;
+
+public class FruitModel
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Display(Name="Fruit Name")]
+    public string? Name { get; set; }
+    [Display(Name ="Available?")]
+    public bool Instock { get; set; }
+}
